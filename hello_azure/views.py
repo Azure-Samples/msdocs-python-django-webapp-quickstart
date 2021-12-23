277ce093e6f2cb10a34b8de9e0fad6c9df6dbe8b
David Berry <dberry83702@yahoo.com>
2021-12-23 16:42:11 -0600

    Initial commit of Django app

---
+++ hello_azure/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+def index(request):
+    return render(request, 'hello_azure/index.html')
+
+def hello(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        
+        if name is None or name == '':
+            print("Request for hello page received with no name or blank name -- redirecting")
+            return redirect('index')
+        else:
+            print("Request for hello page received with name=%s" % name)
+            context = {'name': name }
+            return render(request, 'hello_azure/hello.html', context)
+    else:
+        return redirect('index')

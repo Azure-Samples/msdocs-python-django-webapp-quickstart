5fa174960c25a8a898f81d4ab95360af3818c738
Chris Harris <charris@microsoft.com>
2022-01-18 14:22:26 -0800

    wsgi.py change fixes the initial problem views.py change fixes the subsequent csrf issue

--- quickstartproject/wsgi.py
+++ quickstartproject/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickstartproject.settings')
+settings_module = 'quickstartproject.production' if 'WEBSITE_HOSTNAME' in os.environ else 'quickstartproject.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()

81491a6c58892f22277f05fb882f8b5c667a908a
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2022-12-09 22:11:14 +0530

    Added static root path
    Satatic root will prevent SystemCheckError from popping up at the time of deployment

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+# STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'staticfiles' 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

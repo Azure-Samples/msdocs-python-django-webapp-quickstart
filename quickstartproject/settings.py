7596b99100751f8259ad539feeb9a123b901014e
John Matthew <john@compunique.com>
2023-07-07 10:38:13 -0700

    remove whitenoise for dev

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic",
+    # "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

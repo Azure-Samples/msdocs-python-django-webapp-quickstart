2b4b3ccb5496c224e4fb4eeba1699cb72812f795
Gabriel-n90 <149002075+Gabriel-n90@users.noreply.github.com>
2023-11-14 20:30:46 +0000

    Update models.py

--- hello_azure/models.py
+++ hello_azure/models.py
@@ -1,3 +1,6 @@
+# hello_azure/models.py
 from django.db import models
 
-# Create your models here.
+class YourModel(models.Model):
+    # Your model fields here
+    name = models.CharField(max_length=255)

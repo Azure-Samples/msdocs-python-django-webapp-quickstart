b5e8ae4b35cd09fdc0a596873adc112d2d68607e
praveen72gh <86825261+praveen72gh@users.noreply.github.com>
2023-11-17 20:22:16 +0530

    Update views.py

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -15,7 +15,7 @@ def hello(request):
             print("Request for hello page received with no name or blank name -- redirecting")
             return redirect('index')
         else:
-            print("Request for hello page received with name=%s" % name)
+            print("Request for hello page received with your name=%s" % name)
             context = {'name': name }
             return render(request, 'hello_azure/hello.html', context)
     else:

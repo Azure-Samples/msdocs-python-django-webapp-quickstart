2067a02e1761b59a1e0d0c60bea0ceeeafd50892
Timothy Kunke <Tim@Timothys-MBP.hsd1.ca.comcast.net>
2024-04-04 15:18:25 -0700

    added SCM_DO_BUILD_DURING_DEPLOYMENT to settings

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SCM_DO_BUILD_DURING_DEPLOYMENT = 1
 
 
 # Quick-start development settings - unsuitable for production
